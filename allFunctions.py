bf4d7e44f41e647fd5d3d13888d2f8923ff187b1
SaranyaChattopadhyay <chattopadhyaysaranya@gmail.com>
2021-08-25 00:32:07 +0530

    few changes made to yum funcs.

--- allFunctions.py
+++ allFunctions.py
@@ -1,18 +1,19 @@
 import subprocess
 
 #Configure and check yum configuration on local machine
-def yumConfigurationLocal(ip):
-    subprocess.run(['sudo', 'mkdir', '/dvd'])
-    subprocess.run(['sudo', 'mount', '/dev/cdrom', '/dvd'])
-    subprocess.run(['sudo', 'echo', '"mount /dev/cdrom /dvd"', '>>', '/etc/rc.d/rc.local'])
-    subprocess.run(['sudo', 'chmod', '+x', '/etc/rc.d/rc.local'])
-    subprocess.run('sudo', 'echo', '"[dvd1]"', '>>', '/etc/yum.repo.d/dvd.repo')
-    subprocess.run('sudo', 'echo', '"baseurl=file:///dvd/AppStream"', '>>', '/etc/yum.repo.d/dvd.repo')
-    subprocess.run('sudo', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repo.d/dvd.repo')
-    subprocess.run('sudo', 'echo', '"[dvd2]"', '>>', '/etc/yum.repo.d/dvd.repo')
-    subprocess.run('sudo', 'echo', '"baseurl=file:///dvd/BaseOS"', '>>', '/etc/yum.repo.d/dvd.repo')
-    subprocess.run('sudo', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repo.d/dvd.repo')
-    output = subprocess.run(['yum', 'repolist'])
+def yumConfigurationLocal():
+    #subprocess.run(['sudo', 'mkdir', '-p', '/dvd'])
+    #subprocess.run(['sudo', 'mount', '/dev/cdrom', '/dvd'])
+    #subprocess.run(['sudo', 'echo', '"mount /dev/cdrom /dvd"', '>>', '/etc/rc.d/rc.local'])
+    #subprocess.run(['sudo', 'chmod', '+x', '/etc/rc.d/rc.local'])
+    #subprocess.run(['sudo', 'echo', '"[dvd1]"', '>>', '/etc/yum.repos.d/dvd.repo'])
+    #subprocess.run(['sudo', 'echo', '"baseurl=file:///dvd/AppStream"', '>>', '/etc/yum.repos.d/dvd.repo'])
+    #subprocess.run(['sudo', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repos.d/dvd.repo'])
+    #subprocess.run(['sudo', 'echo', '"[dvd2]"', '>>', '/etc/yum.repos.d/dvd.repo'])
+    #subprocess.run(['sudo', 'echo', '"baseurl=file:///dvd/BaseOS"', '>>', '/etc/yum.repos.d/dvd.repo'])
+    #subprocess.run(['sudo', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repos.d/dvd.repo'])
+    #output = subprocess.run(['sudo', 'yum', 'repolist'])
+    output = subprocess.run(['sudo', 'mkdir', '-p', '/dvd', '&&', 'sudo', 'mount', '/dev/cdrom', '/dvd', '&&', 'sudo', 'echo', '"mount /dev/cdrom /dvd"', '>>', '/etc/rc.d/rc.local', '&&', 'sudo', 'chmod', '+x', '/etc/rc.d/rc.local', '&&', 'sudo', 'echo', '"[dvd1]"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'sudo', 'echo', '"baseurl=file:///dvd/AppStream"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'sudo', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'sudo', 'echo', '"[dvd2]"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'sudo', 'echo', '"baseurl=file:///dvd/BaseOS"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'sudo', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'sudo', 'yum', 'repolist'])
     if output.returncode == 0:
         print("Yum configuration successful!")
     else:
@@ -20,7 +21,7 @@ def yumConfigurationLocal(ip):
 
 #Configure and check yum configuration on remote machine
 def yumConfigureRemote(ip):
-    output = subprocess.run(['ssh', f'root@{ip}', 'mkdir', '/dvd', '&&', 'mount', '/dev/cdrom', '/dvd', '&&', 'echo', '"mount /dev/cdrom /dvd"', '>>', '/etc/rc.d/rc.local', '&&', 'chmod', '+x', '/etc/rc.d/rc.local', '&&', 'echo', '"[dvd1]"', '>>', '/etc/yum.repo.d/dvd.repo', '&&', 'echo', '"baseurl=file:///dvd/AppStream"', '>>', '/etc/yum.repo.d/dvd.repo', '&&', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repo.d/dvd.repo', '&&', 'echo', '"[dvd2]"', '>>', '/etc/yum.repo.d/dvd.repo', '&&', 'echo', '"baseurl=file:///dvd/BaseOS"', '>>', '/etc/yum.repo.d/dvd.repo', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repo.d/dvd.repo', '&&', 'yum', 'repolist'])
+    output = subprocess.run(['ssh', f'root@{ip}', 'mkdir', '-p', '/dvd', '&&', 'mount', '/dev/cdrom', '/dvd', '&&', 'echo', '"mount /dev/cdrom /dvd"', '>>', '/etc/rc.d/rc.local', '&&', 'chmod', '+x', '/etc/rc.d/rc.local', '&&', 'echo', '"[dvd1]"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'echo', '"baseurl=file:///dvd/AppStream"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'echo', '"[dvd2]"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'echo', '"baseurl=file:///dvd/BaseOS"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repos.d/dvd.repo', '&&', 'yum', 'repolist'])
     if output.returncode == 0:
         print("Yum configuration successful!")
     else:
@@ -28,12 +29,12 @@ def yumConfigureRemote(ip):
 
 #Install packages on local machine
 def installPackageLocal(package):
-    output = subprocess.run(['sudo', 'yum', 'install', f'{package}', '-y', '&&', 'systemctl', 'start', 'httpd'])
+    output = subprocess.run(['sudo', 'yum', 'install', f'{package}', '-y'])
     print(output)
 
 #Install packages on remote machine
 def installPackageRemote(ip, package):
-    output = subprocess.run(['ssh', f'root@{ip}', 'yum', 'install', f'{package}', '-y', '&&', 'systemctl', 'start', 'httpd'])
+    output = subprocess.run(['ssh', f'root@{ip}', 'yum', 'install', f'{package}', '-y'])
     print(output)
 
 #Install and start webserver service on localhost
@@ -42,7 +43,7 @@ def webserverLocal():
     print(output)
 
 #Install and start webserver service on remote machine
-def webserverLocal(ip):
+def webserverRemote(ip):
     output = subprocess.run(['ssh', f'root@{ip}', 'yum', 'install', 'httpd', '-y', '&&', 'systemctl', 'start', 'httpd'])
     print(output)
 
@@ -71,7 +72,7 @@ def userDelLocal(username):
         print("User deletion unsuccessful!")
 
 #Delete an existing user user on remote machine
-def userCreateRemote(ip, username):
+def userDelRemote(ip, username):
     output = subprocess.run(['ssh', f'root@{ip}', 'userdel', '-r', f'{username}'])
     if output.returncode == 0:
         print("User deletion successful!")
@@ -96,9 +97,9 @@ def createFolderRemote(ip, folder_path):
 
 #Configure docker on local machine
 def confDockerLocal():
-    subprocess.run(['sudo', 'echo', '"[docker]"', '>>', '/etc/yum.repo.d/dockerRepo.repo'])
-    subprocess.run(['sudo', 'echo', '"baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/"', '>>', '/etc/yum.repo.d/dockerRepo.repo'])
-    subprocess.run(['sudo', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repo.d/dockerRepo.repo'])
+    subprocess.run(['sudo', 'echo', '"[docker]"', '>>', '/etc/yum.repos.d/dockerRepo.repo'])
+    subprocess.run(['sudo', 'echo', '"baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/"', '>>', '/etc/yum.repos.d/dockerRepo.repo'])
+    subprocess.run(['sudo', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repos.d/dockerRepo.repo'])
     subprocess.run(['sudo', 'yum', 'install', 'docker-ce', '--nobest', '-y'])
     output = subprocess.run(['sudo', 'systemctl', 'start', 'docker'])
     if output.returncode == 0:
@@ -108,7 +109,7 @@ def confDockerLocal():
 
 #Configure docker on remote machine
 def confDockerRemote(ip):
-    output = subprocess.run(['ssh', f'root@{ip}', 'echo', '"[docker]"', '>>', '/etc/yum.repo.d/dockerRepo.repo', '&&', 'echo', '"baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/"', '>>', '/etc/yum.repo.d/dockerRepo.repo', '&&', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repo.d/dockerRepo.repo', '&&', 'yum', 'install', 'docker-ce', '--nobest', '-y', '&&', 'systemctl', 'start', 'docker'])
+    output = subprocess.run(['ssh', f'root@{ip}', 'echo', '"[docker]"', '>>', '/etc/yum.repos.d/dockerRepo.repo', '&&', 'echo', '"baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/"', '>>', '/etc/yum.repos.d/dockerRepo.repo', '&&', 'echo', '"gpgcheck=0"', '>>', '/etc/yum.repos.d/dockerRepo.repo', '&&', 'yum', 'install', 'docker-ce', '--nobest', '-y', '&&', 'systemctl', 'start', 'docker'])
     if output.returncode == 0:
         print("Docker configuration successful!")
     else:
@@ -124,10 +125,23 @@ def runDockerRemote(ip, command):
     output = subprocess.getoutput(['ssh', f'root@{ip}', f'{command}'])
     print(output)
 
-
-
-
-
 if __name__ == "__main__":
-    pass
+    yumConfigurationLocal()
+    #yumConfigureRemote("192.168.29.22")
+    #installPackageLocal()
+    #installPackageRemote("192.168.29.22", "git")
+    #webserverLocal()
+    #webserverRemote("192.168.29.22")
+    #userCreateLocal("saptarsi")
+    #userCreateRemote("saptarsi")
+    #userDelLocal("saptarsi")
+    #userDelRemote("saptarsi")
+    #createFolderLocal()
+    #createFolderRemote()
+    #confDockerLocal()
+    #confDockerRemote("192.168.29.22")
+    #runDockerLocal()
+    #runDockerRemote()
+
+
 
